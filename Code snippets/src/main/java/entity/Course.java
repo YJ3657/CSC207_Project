c095ac96282254daa409021490f703a1614a73fc
MatthewYu06 <matthewyu0604@gmail.com>
2023-10-20 12:58:46 -0400

    added entity classes and some attributes(unfinished)

---
+++ Code snippets/src/main/java/entity/Course.java
@@ -0,0 +1,33 @@
+package main.java.entity;
+
+import main.java.entity.Notes;
+import main.java.entity.Contents;
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Course{
+    private String id;
+    private ArrayList<Notes> notes; //is this the best collection to store notes?
+    private HashMap<Integer, Contents> contents;
+
+    public Course(String id){
+        this.id = id;
+        notes = new ArrayList<>();
+        contents = new HashMap<>();
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public ArrayList<Notes> getNotes() {
+        return notes;
+    }
+
+    public HashMap<Integer, Contents> getContents() {
+        return contents;
+    }
+}
