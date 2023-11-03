432803c0113f877ffccbcc8f4d0ddfd7742aaf3f
jerryy2021 <yanjerry1019@gmail.com>
2023-11-03 10:24:55 -0400

    added input dialog box for AddCourse button

--- Code snippets/src/main/java/view/NotesView.java
+++ Code snippets/src/main/java/view/NotesView.java
@@ -6,6 +6,7 @@ import main.java.interface_adapter.notes.NotesViewModel;
 import main.java.interface_adapter.ViewManagerModel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -28,6 +29,38 @@ public class NotesView extends JPanel implements ActionListener, PropertyChangeL
         JLabel title = new JLabel("Notes Screen");
 
         JButton back = new JButton(notesViewModel.BACK_BUTTON_LABEL);
+
+        JButton addCourse = new JButton("Add Course");
+
+        addCourse.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (e.getSource().equals(addCourse)) {
+//                            Icon defaultIcon = UIManager.getIcon("OptionPane.informationIcon");
+                            String s = (String)JOptionPane.showInputDialog(
+                                    NotesView.this,
+                                    "Course ID: \n",
+                                    "Dialog",
+                                    JOptionPane.INFORMATION_MESSAGE,
+                                    null, // TODO: Change this Icon
+                                    null,
+                                    "");
+//
+////If a string was returned, say so.
+//                            if ((s != null) && (s.length() > 0)) {
+//                                setLabel("Green eggs and... " + s + "!");
+//                                return;
+//                            }
+//
+////If you're here, the return value was null/empty.
+//                            setLabel("Come on, finish the sentence!");
+                        }
+                    }
+                }
+        );
+
+
         back.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -37,6 +70,7 @@ public class NotesView extends JPanel implements ActionListener, PropertyChangeL
                 }
             }
         });
+        this.add(addCourse);
         this.add(back);
         this.add(title);
         this.add(notesDisplay);
