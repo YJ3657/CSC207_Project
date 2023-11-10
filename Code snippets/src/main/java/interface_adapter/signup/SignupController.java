5be7b1cfd9a86f6d0569d6eba414fa66348e2af8
zubyogbu <zdogbu@gmail.com>
2023-11-09 19:58:33 -0500

    rough signup interface adapter implementation

--- Code snippets/src/main/java/interface_adapter/signup/SignupController.java
+++ Code snippets/src/main/java/interface_adapter/signup/SignupController.java
@@ -1,4 +1,19 @@
 package main.java.interface_adapter.signup;
 
+import main.java.use_case.signup.SignupInputBoundary;
+import main.java.use_case.signup.SignupInputData;
+
 public class SignupController {
+
+    final SignupInputBoundary userSignupUseCaseInteractor;
+    public SignupController(SignupInputBoundary userSignupUseCaseInteractor) {
+        this.userSignupUseCaseInteractor = userSignupUseCaseInteractor;
+    }
+
+    public void execute(String username, String password1, String password2) {
+        SignupInputData signupInputData = new SignupInputData(
+                username, password1, password2);
+
+        userSignupUseCaseInteractor.execute(signupInputData);
+    }
 }
--- Code snippets/src/main/java/interface_adapter/signup/SignupPresenter.java
+++ Code snippets/src/main/java/interface_adapter/signup/SignupPresenter.java
@@ -1,4 +1,41 @@
 package main.java.interface_adapter.signup;
 
-public class SignupPresenter {
+import main.java.interface_adapter.ViewManagerModel;
+import main.java.interface_adapter.home.HomeState;
+import main.java.interface_adapter.home.HomeViewModel;
+import main.java.use_case.signup.SignupOutputBoundary;
+import main.java.use_case.signup.SignupOutputData;
+
+public class SignupPresenter implements SignupOutputBoundary {
+    private final SignupViewModel signupViewModel;
+    private final HomeViewModel homeViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel,
+                           HomeViewModel homeViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.homeViewModel = homeViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        // On success, switch to the home view.
+
+        HomeState homeState = homeViewModel.getState();
+        homeState.setUsername(response.getUsername());
+        this.homeViewModel.setState(homeState);
+        this.homeViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(homeViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(error);
+        signupViewModel.firePropertyChanged();
+    }
 }
