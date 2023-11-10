908382b1fb1bb00d0dbd3d10b1f581ee16a1e449
zubyogbu <zdogbu@gmail.com>
2023-11-09 19:39:42 -0500

    rough login interface adapter implementation

--- Code snippets/src/main/java/interface_adapter/login/LoginPresenter.java
+++ Code snippets/src/main/java/interface_adapter/login/LoginPresenter.java
@@ -1,4 +1,41 @@
 package main.java.interface_adapter.login;
 
-public class LoginPresenter {
+import main.java.interface_adapter.ViewManagerModel;
+import main.java.interface_adapter.home.HomeState;
+import main.java.interface_adapter.home.HomeViewModel;
+import main.java.use_case.login.LoginOutputBoundary;
+import main.java.use_case.login.LoginOutputData;
+
+public class LoginPresenter implements LoginOutputBoundary {
+    private final LoginViewModel loginViewModel;
+    private final HomeViewModel homeViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          HomeViewModel homeViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.homeViewModel = homeViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the home view.
+
+        HomeState homeState = homeViewModel.getState();
+        homeState.setUsername(response.getUsername());
+        this.homeViewModel.setState(homeState);
+        this.homeViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(homeViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsernameError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
