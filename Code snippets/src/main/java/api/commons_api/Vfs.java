26418e0bc985cf2873ed056e6227e2863d86b8ae
MatthewYu06 <matthewyu0604@gmail.com>
2023-11-10 23:18:24 -0500

    Created find files test

--- Code snippets/src/main/java/api/commons_api/Vfs.java
+++ Code snippets/src/main/java/api/commons_api/Vfs.java
@@ -6,16 +6,13 @@ import org.apache.commons.vfs2.*;
 public class Vfs {
     public static void main(String[] args) throws FileSystemException {
         FileSystemManager fsManager = VFS.getManager();
-        String relFilePath = "Code snippets\\src\\main\\java\\api\\commons_api\\test.txt";
+        String relFilePath = "Code snippets\\src\\main\\java\\api\\commons_api\\tes.txt";
         String absFilePath = System.getProperty("user.dir") + "\\" + relFilePath;
         FileObject file = fsManager.resolveFile(absFilePath);
 
-        if (file.exists()){
-            System.out.println(file.getContent().getSize());
+        System.out.println(file.getContent().getSize());
 
-        }else{
-            System.out.println("File not found");
-        }
+
 
     }
 }
---
+++ Code snippets/test/FindFilesTest.java
@@ -0,0 +1,43 @@
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemException;
+import org.apache.commons.vfs2.FileSystemManager;
+import org.apache.commons.vfs2.VFS;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FindFilesTest {
+    private FileSystemManager fsm;
+
+    @Before
+    public void init() throws FileSystemException {
+        fsm = VFS.getManager();
+    }
+
+    @Test
+    public void testFileSizeInFolder() throws FileSystemException {
+        String relFilePath = "Code snippets\\src\\main\\java\\api\\commons_api\\test.txt";
+        String absFilePath = System.getProperty("user.dir") + "\\" + relFilePath;
+        FileObject file = fsm.resolveFile(absFilePath);
+        long actual = 0;
+        if (file.exists()){
+            actual = file.getContent().getSize();
+        }
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void testFileSystemException(){
+        try{
+            String relFilePath = "Code snippets\\src\\main\\java\\api\\commons_api\\fraud.txt";
+            String absFilePath = System.getProperty("user.dir") + "\\" + relFilePath;
+            FileObject file = fsm.resolveFile(absFilePath);
+
+        } catch (FileSystemException e) {
+            // Expected
+        }
+
+
+    }
+    }
