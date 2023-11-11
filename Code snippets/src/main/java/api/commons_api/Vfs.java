ed9f4fa591c9a368161d24d07d654b37034bc41c
MatthewYu06 <matthewyu0604@gmail.com>
2023-11-10 19:28:19 -0500

    added getters/setters to HomeState and HomeViewModel so program can run, AND set up pom.xml file so apache vfs test works

--- Code snippets/src/main/java/api/commons_api/Vfs.java
+++ Code snippets/src/main/java/api/commons_api/Vfs.java
@@ -1,15 +1,14 @@
 package main.java.api.commons_api;
 
+import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.*;
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.FileSystemManager;
-import org.apache.commons.vfs2.VFS;
 
 public class Vfs {
     public static void main(String[] args) throws FileSystemException {
         FileSystemManager fsManager = VFS.getManager();
-        String filePath = "file:///src/main.java/api/commons_api/test.txt";
-        FileObject file = fsManager.resolveFile(filePath);
+        String relFilePath = "Code snippets\\src\\main\\java\\api\\commons_api\\test.txt";
+        String absFilePath = System.getProperty("user.dir") + "\\" + relFilePath;
+        FileObject file = fsManager.resolveFile(absFilePath);
 
         if (file.exists()){
             System.out.println(file.getContent().getSize());
