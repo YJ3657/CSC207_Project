0cd28965aa6fd984745b85ed4a151dddeea2d29f
MatthewYu06 <matthewyu0604@gmail.com>
2023-11-17 16:48:07 -0500

    in the middle of implementing QuizView.java, want to save progress

--- Code snippets/test/ConnectionExample.java
+++ Code snippets/test/ConnectionExample.java
@@ -1,33 +1,33 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class ConnectionExample {
-    public static void main(String[] args) {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://csc207:3306/thisisjava",
-                    "remoteUser",
-                    "thisismysql*"
-            );
-            System.out.println("Connection Success");
-
-
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            if(conn != null) {
-                try {
-                    conn.close();
-                    System.out.println("Connection cut");
-                } catch (SQLException e) {}
-            }
-        }
-    }
-
-}
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//
+//public class ConnectionExample {
+//    public static void main(String[] args) {
+//        Connection conn = null;
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//            conn = DriverManager.getConnection(
+//                    "jdbc:mysql://csc207:3306/thisisjava",
+//                    "remoteUser",
+//                    "thisismysql*"
+//            );
+//            System.out.println("Connection Success");
+//
+//
+//        } catch (ClassNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if(conn != null) {
+//                try {
+//                    conn.close();
+//                    System.out.println("Connection cut");
+//                } catch (SQLException e) {}
+//            }
+//        }
+//    }
+//
+//}
