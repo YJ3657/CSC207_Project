93729890e4492bc6d6e55ea3c69a9c65c020050e
zubyogbu <zdogbu@gmail.com>
2023-11-17 21:11:49 -0500

    Implemented create notes use case

--- Code snippets/src/main/java/use_case/notes/CreateNotesOutputData.java
+++ Code snippets/src/main/java/use_case/notes/CreateNotesOutputData.java
@@ -1,2 +1,18 @@
-package main.java.use_case.notes;public class CreateNotesOutputData {
+package main.java.use_case.notes;
+
+import main.java.entity.Notes;
+
+public class CreateNotesOutputData {
+    private final Notes notes;
+
+    private boolean useCaseFailed;
+
+    public CreateNotesOutputData(Notes notes, boolean useCaseFailed) {
+        this.notes = notes;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
 }
