d3c9816aa5d56825549db97ca319ffb7a9c40454
YJ3657 <yeongjay2@gmail.com>
2023-12-07 15:45:16 -0500

    commit for pull request

--- Code snippets/src/main/java/use_case/reminder/ReminderInputBoundary.java
+++
@@ -1,9 +0,0 @@
-package main.java.use_case.reminder;
-
-import main.java.entity.Reminder;
-
-import java.util.Map;
-
-public interface ReminderInputBoundary {
-    public Map<String, Reminder> execute(ReminderInputData reminderInputData);
-}
--- Code snippets/src/main/java/use_case/reminder/ReminderOutputBoundary.java
+++
@@ -1,8 +0,0 @@
-package main.java.use_case.reminder;
-
-public interface ReminderOutputBoundary {
-
-    public void prepareSuccessView(ReminderOutputData reminderOutputData);
-
-    public void prepareFailView(String error);
-}
