3e4caad251030e81849784aa6a5583d08921ab91
YJ3657 <yeongjay2@gmail.com>
2023-12-07 02:17:10 -0500

    Implement the ChatBot UI

---
+++ Code snippets/src/main/java/view/ChatBot/swing/scroll/ScrollBar.java
@@ -0,0 +1,14 @@
+package main.java.view.ChatBot.swing.scroll;
+
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+public class ScrollBar extends JScrollBar {
+
+    public ScrollBar() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(5, 5));
+        setOpaque(false);
+        setUnitIncrement(20);
+    }
+}
